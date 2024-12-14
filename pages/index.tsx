349372a687d9856c7c0778e529e0959e942a191f
abayomi <king.aba@gmail.com>
2024-12-14 17:45:58 -0500

    Update index.tsx
    const e added just to get a change in

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
+const e = 'f'
 export default function Home() {
   return (
     <>
